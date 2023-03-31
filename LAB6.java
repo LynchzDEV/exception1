da31e7ff1b7cc7a01808f191b8625366788549ca
LynchzDEV <opmok48@gmail.com>
2023-03-31 10:25:59 +0700

    lab 4-7 and MyException

---
+++ LAB6.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception1;
+
+/**
+ *
+ * @author Dell
+ */
+public class LAB6 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(1/0);
+        }
+        catch (ArithmeticException e) {
+//            e.printStackTrace();
+              System.out.println(e.getMessage());
+              System.out.println(e.toString());  
+              StackTraceElement[] st = e.getStackTrace();
+              for (int i = 0; i < st.length; i++) {
+              System.out.println(st[0].getClassName());
+              System.out.println(st[0].getMethodName());
+              System.out.println(st[0].getFileName());
+              System.out.println(st[0].getLineNumber());
+              }
+        }
+        
+    }
+}
